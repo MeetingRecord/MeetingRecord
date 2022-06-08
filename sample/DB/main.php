f90952c26a2f2040caf89d76befa4a0508e78fe6
Shu-Matsumoto <motomashu@gmail.com>
2022-06-08 12:08:21 +0900

    lolipopマネージドクラウドサーバで動作させるPHPファイルサンプル作成 　ーユーザ登録 　ーユーザログイン

---
+++ sample/DB/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>メイン画面</title>
+</head>
+<body>
+  <h1>メイン画面</h1>
+</body>
+
+</html>
